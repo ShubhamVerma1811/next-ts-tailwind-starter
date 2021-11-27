8d1c07967e600cbe9ffa31dda86a4d9a6ce0fc5e
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2021-11-27 10:57:21 +0530

    Revert "chore: init"
    This reverts commit 9edf67f20e26309247b03b53e69a268b12d92181.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
+const { fuchsia } = require('tailwindcss/colors');
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        fuchsia,
+      },
+    },
   },
   variants: {
     extend: {},

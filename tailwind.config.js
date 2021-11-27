9edf67f20e26309247b03b53e69a268b12d92181
Shubham Verma <25576658+ShubhamVerma1811@users.noreply.github.com>
2021-11-27 10:56:45 +0530

    chore: init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,8 @@
-const { fuchsia } = require('tailwindcss/colors');
-
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {
-      colors: {
-        fuchsia,
-      },
-    },
+    extend: {},
   },
   variants: {
     extend: {},
